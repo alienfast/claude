a60b3b6a4f4d451f5671a9b3e84e4b29f9054a89
Kevin Ross <kevin.ross@alienfast.com>
2025-09-12 17:47:39 -0500

    setup eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import af from '@alienfast/eslint-config'
+import { defineConfig } from 'eslint/config'
+
+/**
+ * Project eslint configuration.
+ *
+ * View config with `npx @eslint/config-inspector`
+ */
+export default defineConfig([
+  af.configs.recommended,
+  {
+    name: 'project',
+    rules: {
+      'markdown/no-missing-label-refs': 'off', // too many false positives on .claude files, move up to preset.
+    },
+  },
+  { ignores: ['.storybook'] },
+])
